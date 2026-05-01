2dff03d1583e69109e19096e32f0c5c39f7287ad
ArthurMascaro <arthurdevicentemascaro31@gmail.com>
2026-05-01 15:49:49 -0300

    feat: create the app header

--- app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/ui/FastTripPlannerApp.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/ui/FastTripPlannerApp.kt
@@ -1,17 +1,52 @@
 package br.edu.ifsp.scl.sc3044599.fasttripplanner.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FastTripPlannerApp() {
-    Column(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
-        Text(text = "oi")
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+        ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+
+        ) {
+            Text(
+                text = "Fast Trip Planner",
+                fontSize = 20.sp,
+                style = TextStyle(fontWeight = FontWeight.Bold)
+            )
+            HorizontalDivider()
+        }
     }
 }
 
