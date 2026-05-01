e7807d4fb418dd7aa64eb9ff8ae45d3cad5313b6
ArthurMascaro <arthurdevicentemascaro31@gmail.com>
2026-05-01 18:08:39 -0300

    refactor: change TripIntentKeys back to an object

--- app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/domain/TripIntentKeys.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/domain/TripIntentKeys.kt
@@ -1,7 +1,7 @@
 package br.edu.ifsp.scl.sc3044599.fasttripplanner.domain
 
-enum class TripIntentKeys(val key: String) {
-    DESTINATION("destination"),
-    DAYS("days"),
-    DAILY_BUDGET("daily_budget");
+object TripIntentKeys {
+    const val DESTINATION = "destination"
+    const val DAYS = "days"
+    const val DAILY_BUDGET = "daily_budget"
 }
