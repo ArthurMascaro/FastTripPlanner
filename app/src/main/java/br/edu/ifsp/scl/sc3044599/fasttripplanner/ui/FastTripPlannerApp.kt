0029ec76ee75b859f92a246a9e9da5076155cf91
ArthurMascaro <arthurdevicentemascaro31@gmail.com>
2026-05-01 16:32:24 -0300

    feat: create the destination form with a reset button

--- app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/ui/FastTripPlannerApp.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044599/fasttripplanner/ui/FastTripPlannerApp.kt
@@ -2,13 +2,17 @@ package br.edu.ifsp.scl.sc3044599.fasttripplanner.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
@@ -21,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,15 +41,17 @@ fun FastTripPlannerApp() {
     var dailyBudget: Double by rememberSaveable() { mutableDoubleStateOf(0.0) }
     var dailyBudgetError: String? by rememberSaveable() { mutableStateOf(null) }
 
+    val maxWidthModifier = Modifier.fillMaxWidth();
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
         ) {
+        // <-- Header -->
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
+            modifier = maxWidthModifier
                 .padding(top = 8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
 
@@ -56,6 +63,103 @@ fun FastTripPlannerApp() {
             )
             HorizontalDivider()
         }
+
+       Column(
+            modifier = maxWidthModifier
+                .padding(top = 20.dp)
+       ) {
+           Column(
+               maxWidthModifier
+           ) {
+               Text(
+                   text = "Informe o destino da viagem",
+                   fontSize = 16.sp
+               )
+               TextField(
+                   value = destination,
+                   onValueChange = { destination = it },
+                   isError = destinationError != null,
+                   placeholder = { Text(text = "Rio de Janeiro") },
+                   maxLines = 2,
+                   supportingText = {
+                       if(destinationError != null){
+                           Text(destinationError!!)
+                       }
+                   },
+                   modifier = Modifier.fillMaxWidth()
+               )
+           }
+
+           Column(
+               maxWidthModifier
+           ) {
+               Text(
+                   text = "Informe a duração da viagem (em dias)",
+                   fontSize = 16.sp
+               )
+               TextField(
+                   value = if(days == 0) "" else days.toString(),
+                   onValueChange = { days = it.toIntOrNull() ?: 0 },
+                   keyboardOptions = KeyboardOptions(
+                       keyboardType = KeyboardType.Number
+                   ),
+                   isError = daysError != null,
+                   placeholder = { Text(text = "5") },
+                   maxLines = 1,
+                   supportingText = {
+                       if(daysError != null){
+                           Text(daysError!!)
+                       }
+                   },
+                   modifier = Modifier.fillMaxWidth()
+               )
+           }
+
+           Column(
+               maxWidthModifier
+           ) {
+               Text(
+                   text = "Informe o orçamento diário (em R$)",
+                   fontSize = 16.sp
+               )
+               TextField(
+                   value = if(dailyBudget == 0.0) "" else dailyBudget.toString(),
+                   onValueChange = { dailyBudget = it.toDoubleOrNull() ?: 0.0 },
+                   keyboardOptions = KeyboardOptions(
+                       keyboardType = KeyboardType.Number
+                   ),
+                   isError = dailyBudgetError != null,
+                   placeholder = { Text(text = "100.00") },
+                   maxLines = 1,
+                   supportingText = {
+                       if(dailyBudgetError != null){
+                           Text(dailyBudgetError!!)
+                       }
+                   },
+                   modifier = Modifier.fillMaxWidth()
+               )
+           }
+
+           Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier
+                     .fillMaxWidth()
+                     .padding(top = 16.dp)
+           ) {
+               Button(
+                   onClick = {
+                       destination = ""
+                       destinationError = null
+                       days = 0
+                       daysError = null
+                       dailyBudget = 0.0
+                       dailyBudgetError = null
+                   }
+               ) {
+                   Text("Limpar")
+               }
+           }
+       }
     }
 }
 
